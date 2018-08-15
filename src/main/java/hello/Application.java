5155634c1fda0b3af10e899cf83284604aea3655
Thomas Ersfeld <thomas.ersfeld@capco.com>
2018-08-15 09:05:14 +0100

    Refactoring

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,54 +1,4 @@
 package hello;
-//
-//import com.mongodb.client.FindIterable;
-//import org.bson.types.ObjectId;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//
-//import static com.google.common.collect.Lists.newArrayList;
-//import static org.springframework.web.bind.annotation.RequestMethod.GET;
-//import static springfox.documentation.schema.AlternateTypeRules.newRule;
-//
-//import java.time.LocalDate;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.context.request.async.DeferredResult;
-//
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.builders.ResponseMessageBuilder;
-//import springfox.documentation.schema.ModelRef;
-//import springfox.documentation.schema.WildcardType;
-//import springfox.documentation.service.ApiKey;
-//import springfox.documentation.service.AuthorizationScope;
-//import springfox.documentation.service.SecurityReference;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spi.service.contexts.SecurityContext;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger.web.SecurityConfiguration;
-//import springfox.documentation.swagger.web.UiConfiguration;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import com.mongodb.MongoClient;
-//
-//import com.mongodb.client.MongoDatabase;
-//import com.mongodb.client.MongoCollection;
-//
-//import org.bson.Document;
-//
-//import static com.mongodb.client.model.Filters.*;
-//
-//import java.util.List;
-//
-//import org.json.JSONObject;
-//
-//import com.fasterxml.classmate.TypeResolver;
-//
-//import javax.annotation.PostConstruct;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
@@ -57,18 +7,32 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.List;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static com.mongodb.client.model.Filters.eq;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -101,12 +65,7 @@ public class Application {
     @RequestMapping("/users/list")
     public String usersList(){
         MongoCollection<Document> collection = database.getCollection("test");
-
-        // Getting the iterable object
         FindIterable<Document> iterDoc = collection.find();
-        int i = 1;
-
-        // Getting the iterator
 
         StringBuilder sb = new StringBuilder();
 
@@ -148,42 +107,42 @@ public class Application {
     }
 
 
-//    @Bean
-//    public Docket petApi() {
-//        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().pathMapping("/")
-//                .directModelSubstitute(LocalDate.class, String.class).genericModelSubstitutes(ResponseEntity.class)
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().pathMapping("/")
+                .directModelSubstitute(LocalDate.class, String.class).genericModelSubstitutes(ResponseEntity.class)
 //                .alternateTypeRules(newRule(typeResolver.resolve(DeferredResult.class, typeResolver.resolve(ResponseEntity.class, WildcardType.class)), typeResolver.resolve(WildcardType.class)))
-//                .useDefaultResponseMessages(false)
-//                .globalResponseMessage(GET, newArrayList(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Error")).build()))
-//                .securitySchemes(newArrayList(apiKey())).securityContexts(newArrayList(securityContext()));
-//    }
-//
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(GET, newArrayList(new ResponseMessageBuilder().code(500).message("500 message").responseModel(new ModelRef("Error")).build()))
+                .securitySchemes(newArrayList(apiKey())).securityContexts(newArrayList(securityContext()));
+    }
+
 //    @Autowired
 //    private TypeResolver typeResolver;
-//
-//    private ApiKey apiKey() {
-//        return new ApiKey("mykey", "api_key", "header");
-//    }
-//
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/anyPath.*")).build();
-//    }
-//
-//    List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return newArrayList(new SecurityReference("mykey", authorizationScopes));
-//    }
-//
-//    @Bean
-//    SecurityConfiguration security() {
-//        return new SecurityConfiguration("test-app-client-id", "test-app-realm", "test-app", "apiKey");
-//    }
-//
-//    @Bean
-//    UiConfiguration uiConfig() {
-//        return new UiConfiguration("validatorUrl");
-//    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("mykey", "api_key", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.regex("/anyPath.*")).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return newArrayList(new SecurityReference("mykey", authorizationScopes));
+    }
+
+    @Bean
+    SecurityConfiguration security() {
+        return new SecurityConfiguration("test-app-client-id", "test-app-realm", "test-app", "apiKey");
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration("validatorUrl");
+    }
 
 }
